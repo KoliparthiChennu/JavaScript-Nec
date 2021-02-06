021ebf72164fe414ba909a63b935cad4917d14fc
chennu <kchennuk7@gmail.com>
2021-02-06 12:32:26 +0530

    updated js files

--- js/fetchData.js
+++ js/fetchData.js
@@ -5,13 +5,14 @@ fetch('json/data.json')
     other(d.multipleData);
   })
   var root = document.getElementById("root");
+  root.setAttribute("class","card");
   function details(cse){
     var n = document.createElement("h1");
-    n.setAttribute("class","name");
+    n.setAttribute("class","text-success");
     n.textContent = cse.name;
     root.appendChild(n);
     var n1 = document.createElement("p");
-    n1.setAttribute("class","name");
+    n1.setAttribute("class","text-info");
     n1.textContent = cse.mobile;
     root.appendChild(n1);
   }
@@ -20,10 +21,12 @@ fetch('json/data.json')
     root.appendChild(ul);
     for(let i = 0; i < r.length; i++){
         var li = document.createElement("li");
+        li.setAttribute("class","bg-success");
         //li.style.fontSize = "20px";
         li.textContent = r[i].name;
         ul.appendChild(li);
         var li1 = document.createElement("li");
+        li1.setAttribute("class","bg-info");
         li1.textContent = r[i].mobile;
         ul.appendChild(li1);
     }
